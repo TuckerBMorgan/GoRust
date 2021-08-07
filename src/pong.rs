668fead72813737aeaa1309bc984cba5abf147d7
Tucker Morgan <Tucker.bull.morgan@gmail.com>
2021-08-07 15:50:32 -0700

    Many stones

--- src/pong.rs
+++ src/pong.rs
@@ -1,6 +1,8 @@
 use amethyst::{
     assets::{AssetStorage, Loader, Handle},
     core::transform::Transform,
+    core::math::{Vector3
+    },
     prelude::*,
     renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
     ecs::{Component, DenseVecStorage, Entity},
@@ -13,7 +15,7 @@ pub const ARENA_HEIGHT: f32 = 100.0;
 pub const ARENA_WIDTH: f32 = 100.0;
 
 pub const PADDLE_HEIGHT: f32 = 16.0;
-pub const PADDLE_WIDTH: f32 = 4.0;
+pub const PADDLE_WIDTH: f32 = 32.0;
 
 pub const BALL_VELOCITY_X: f32 = 75.0;
 pub const BALL_VELOCITY_Y: f32 = 50.0;
@@ -94,79 +96,17 @@ impl SimpleState for Pong {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
         world.register::<Stone>();
-        self.ball_spawn_timer.replace(1.0f32);
-        self.sprite_sheet_handle.replace(load_sprite_sheet(world));
-        //initialise_paddles(world, load_stone_sprite_sheet(world));
+
+        //self.sprite_sheet_handle.replace(load_sprite_sheet(world));
         let sr = load_stone_sprite_sheet(world);
         initialise_stones(world, sr);
         initialise_camera(world);
-        //initialise_scoreboard(world);
     }
 
     fn update(&mut self, data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
-        if let Some(mut timer) = self.ball_spawn_timer.take() {
-            // If the timer isn't expired yet, subtract the time that passed since the last update.
-            {
-                let time = data.world.fetch::<Time>();
-                timer -= time.delta_seconds();
-            }
-            if timer <= 0.0 {
-                // When timer expire, spawn the ball
-                //initialise_ball(data.world, self.sprite_sheet_handle.clone().unwrap());
-            } else {
-                // If timer is not expired yet, put it back onto the state.
-                self.ball_spawn_timer.replace(timer);
-            }
-        }
         Trans::None
     }
 
-}
-
-/// Initialises a ui scoreboard
-fn initialise_scoreboard(world: &mut World) {
-    let font = world.read_resource::<Loader>().load(
-        "font/square.ttf",
-        TtfFormat,
-        (),
-        &world.read_resource(),
-    );
-    let p1_transform = UiTransform::new(
-        "P1".to_string(), Anchor::TopMiddle, Anchor::TopMiddle,
-        -50., -50., 1., 200., 50.,
-    );
-    let p2_transform = UiTransform::new(
-        "P2".to_string(), Anchor::TopMiddle, Anchor::TopMiddle,
-        50., -50., 1., 200., 50.,
-    );
-
-    let p1_score = world
-        .create_entity()
-        .with(p1_transform)
-        .with(UiText::new(
-            font.clone(),
-            "0".to_string(),
-            [1., 1., 1., 1.],
-            50.,
-            LineMode::Single,
-            Anchor::Middle,
-        ))
-        .build();
-
-    let p2_score = world
-        .create_entity()
-        .with(p2_transform)
-        .with(UiText::new(
-            font,
-            "0".to_string(),
-            [1., 1., 1., 1.],
-            50.,
-            LineMode::Single,
-            Anchor::Middle,
-        ))
-        .build();
-
-    world.insert(ScoreText { p1_score, p2_score });
 }
 
 fn initialise_camera(world: &mut World) {
@@ -181,100 +121,22 @@ fn initialise_camera(world: &mut World) {
         .build();
 }
 
-/// Initialises one ball in the middle-ish of the arena.
-fn initialise_ball(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
-    // Create the translation.
-    let mut local_transform = Transform::default();
-    local_transform.set_translation_xyz(ARENA_WIDTH / 2.0, ARENA_HEIGHT / 2.0, 0.0);
-
-    // Assign the sprite for the ball. The ball is the second sprite in the sheet.
-    let sprite_render = SpriteRender::new(sprite_sheet_handle, 1);
-
-    world
-        .create_entity()
-        .with(sprite_render)
-        .with(Ball {
-            radius: BALL_RADIUS,
-            velocity: [BALL_VELOCITY_X, BALL_VELOCITY_Y],
-        })
-        .with(local_transform)
-        .build();
-}
-
-fn initialise_paddles(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
-
-    let sprite_render = SpriteRender::new(sprite_sheet_handle, 0);
-
-    let mut left_transform = Transform::default();
-    let mut right_transform = Transform::default();
-
-    // Correctly position the paddles.
-    let y = ARENA_HEIGHT / 2.0;
-    left_transform.set_translation_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
-    right_transform.set_translation_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
-
-    // Create a left plank entity.
-    world
-        .create_entity()
-        .with(sprite_render.clone())
-        .with(Paddle::new(Side::Left))
-        .with(left_transform)
-        .build();
-
-    // Create right plank entity.
-    world
-        .create_entity()
-        .with(sprite_render.clone())
-        .with(Paddle::new(Side::Right))
-        .with(right_transform)
-        .build();
-}
 
 fn initialise_stones(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
     let sprite_render = SpriteRender::new(sprite_sheet_handle, 0);
+    for i in 0.. 3 {
+        let mut left_transform = Transform::default();
+        left_transform.set_translation_xyz(PADDLE_WIDTH * i as f32, ARENA_WIDTH / 2.0f32, 0.0);
+        let scale = Vector3::new(0.5, 0.5, 1.0);
+        left_transform.set_scale(scale);
 
-    let mut left_transform = Transform::default();
-    let mut right_transform = Transform::default();
-    // Correctly position the paddles.
-    let y = ARENA_HEIGHT / 2.0;
-    left_transform.set_translation_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
-    right_transform.set_translation_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
-
-
-    world
-        .create_entity()
-        .with(sprite_render.clone())
-        .with(Stone::new(Side::Left))
-        .with(left_transform)
-        .build();
-    
-}
-
-fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
-    // Load the sprite sheet necessary to render the graphics.
-    // The texture is the pixel data
-    // `texture_handle` is a cloneable reference to the texture
-    let texture_handle = {
-        let loader = world.read_resource::<Loader>();
-        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
-        loader.load(
-            "texture/pong_spritesheet.png",
-            ImageFormat::default(),
-            (),
-            &texture_storage,
-        )
-    };
-
-    let loader = world.read_resource::<Loader>();
-    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
-    loader.load(
-        "texture/pong_spritesheet.ron", // Here we load the associated ron file
-        SpriteSheetFormat(texture_handle),
-        (),
-        &sprite_sheet_store,
-    )
-
-    //...
+        world
+            .create_entity()
+            .with(sprite_render.clone())
+            .with(Stone::new(Side::Left))
+            .with(left_transform)
+            .build();
+    }    
 }
 
 fn load_stone_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
